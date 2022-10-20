2d00a8c37e5c129ca2ab171a5a124e0eb4d65b85
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-20 20:36:15 +0530

    Add files via upload

---
+++ pythond422.py
@@ -0,0 +1,59 @@
+
+x = ("abhi", "yashmine", "sabhu", "satheesh", "vinay")
+print(x)
+# int
+x = (10)
+y = int(2)
+z = float(10)
+print(x, y, z)
+print(x+y)
+# exercise
+x = 78
+y = ("eren yeger")
+print(type(x))
+print(type(y))
+print(y.replace("eren", "EREN"))
+# slice string
+x = "our either die a hero, or you live long enough to see yourself become the villain. .."
+y = "The past can talk to the future... if the present has ears to hear and eyes to see with."
+print(x.upper())
+print(x.lower())
+print(y.lower())
+print(y.upper())
+print(type(x))
+print(type(y))
+print(x.replace("e", "o"))
+print(y.replace("r", "u"))
+print(y.replace("k", "p"))
+print(y.replace("h", "k"))
+print(y.replace("l", "r"))
+print(y.replace("y", "t"))
+print(y.replace("a", "q"))
+print(y.replace("t", "c"))
+print(y.replace("d", "k"))
+print(y.replace("k", "t"))
+print(x.split())
+print(x.split())
+# mylist properties
+mylist = [40, 20, 43, 56, 98, 89, 90, 87]
+mylist[1:2] = [58, 300]
+mylist[1:5] = [2000, 300]
+mylist[0:4] = [200, 300]
+mylist[0:6] = [200, 3000]
+mylist[1:3] = [200, 300]
+mylist[-6:2] = [200, 300]
+mylist[1:-8] = [200, 300]
+mylist[1:-6] = [200, 300]
+mylist[-1:6] = [200, 300]
+print(mylist)
+mylist.clear()
+print(mylist)
+# arithmetic operator
+print(100+10)
+print(1056-20)
+print(1090-20)
+print(107*20)
+print(106/20)
+print(105//20)
+print(105 % 20)
+print(109**20)
